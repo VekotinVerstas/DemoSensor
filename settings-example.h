1fab64f4ed69e54e9fa602fdb5e9377f70faa0ec
Aapo Rista <aapo.gitpub@rista.net>
2018-08-31 10:32:21 +0300

    Add example settings

---
+++ settings-example.h
@@ -0,0 +1,13 @@
+// MQTT settings
+#define MQTT_TOPIC "esine"
+#define MQTT_SERVER "mqtt.example.org"
+#define MQTT_PORT 1883
+#define MQTT_USER "mqtt_user_with_read-write_permission_to_topic"
+#define MQTT_PASSWORD "mqtt_password"
+
+// Sensor settings
+#define BME680_HEATING_TIME 150 // milliseconds
+#define BME680_SEND_DELAY 10000 // milliseconds
+
+#define BH1750_SEND_DELAY 1000  // milliseconds
+
