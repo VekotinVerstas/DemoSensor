1b73d1c5add77789144e31f59a1dd80c621c3298
Aapo Rista <aapo.gitpub@rista.net>
2019-09-20 13:25:40 +0300

    Reorganise settings and add configurable BURK_ID

--- settings-example.h
+++ settings-example.h
@@ -1,3 +1,8 @@
+// AP name prefix
+#define AP_PREFIX "AQBURK"
+// BURK_ID - change this in every device!!!
+#define BURK_ID "800001"
+
 // MQTT settings
 #define MQTT_TOPIC "demosensor"
 #define MQTT_SERVER "mqtt.example.org"
@@ -6,39 +11,45 @@
 #define MQTT_PASSWORD "mqtt_password"
 #define MQTT_MAX_PACKET_SIZE 256 // For pubsubclient, default is 128 B
 
-#define SENSOR_SEND_MAX_DELAY 60000 // milliseconds, after this send data anyway even it has not changed at all
+#define SENSOR_SEND_MAX_DELAY 60000 // milliseconds, after this send data anyway even it has not changed at all, good values are between 1-10 minutes
 #define STATUS_SEND_DELAY 60000
 
 // Sensor settings
 
 // Buttons (digital HIGH / LOW)
+// #define BUTTON_USE  // Comment out if there are no button-like sensors
 #define PUSHBUTTON_1 D7
 #define PUSHBUTTON_2 D8
 #define PUSHBUTTON_SEND_DELAY 100
 
+// Basic I2C sensors
+#define BH1750_SEND_DELAY 1000  // milliseconds
+#define MLX90614_SEND_DELAY 1000  // milliseconds
+#define APDS9960_SEND_DELAY 1000  // milliseconds
+#define SI7021_SEND_DELAY 1000  // milliseconds
+#define BME280_SEND_DELAY 1000 // milliseconds
+#define SHT31_SEND_DELAY 1000  // milliseconds
+
+// BME680
 #define BME680_HEATING_TIME 150 // milliseconds
 #define BME680_SEND_DELAY 10000 // milliseconds
 
-#define BH1750_SEND_DELAY 1000  // milliseconds
+// Dallas DS18B20 settings
+#define DS18B20_SEND_DELAY 2000 // milliseconds
+#define ONE_WIRE_BUS D3         // data pin (a 4.7-10K pull-up resistor is necessary, wemos mini has it built-in on D3, D4)
 
-#define MLX90614_SEND_DELAY 1000  // milliseconds
-
-#define APDS9960_SEND_DELAY 1000  // milliseconds
-
-#define SI7021_SEND_DELAY 1000  // milliseconds
-
-#define BME280_SEND_DELAY 1000 // milliseconds
-
-#define SDS011_SEND_DELAY 500 // milliseconds
-#define SDS011_RXPIN D3
-#define SDS011_TXPIN D4
-
-//#define MHZ19_USE  // Comment out if MHZ19 is not connected
+// MHZ19 CO2 sensor
+// #define MHZ19_USE  // Comment out if MHZ19 is not connected
 #define MHZ19_SEND_DELAY 5000 // milliseconds
 #define MHZ19_RXPIN D5
 #define MHZ19_TXPIN D6
 
-#define DS18B20_SEND_DELAY 2000 // milliseconds
-#define ONE_WIRE_BUS D3         // data pin (a 4.7-10K pull-up resistor is necessary, wemos mini has it built-in on D3, D4)
-
+// K-type thermocouple sensor with maxNNNN amplifier
+// #define THERMO_USE  // Comment out if MAX6675 or MAX31855 is not connected
 #define THERMO_SEND_DELAY 2000 // milliseconds
+
+// Nova SDS011 particulate matter (PM) sensor
+// #define SDS011_USE // Comment out if SDS011 is not connected
+#define SDS011_SEND_DELAY 500 // milliseconds
+#define SDS011_RXPIN 14
+#define SDS011_TXPIN 12
