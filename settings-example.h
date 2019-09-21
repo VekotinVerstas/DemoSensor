8f1c87c11adb043abc9e809b0dec2cc516674e77
Aapo Rista <aapo.gitpub@rista.net>
2019-09-21 10:51:56 +0300

    Add info about HTTP and MQTT protocols

--- settings-example.h
+++ settings-example.h
@@ -1,18 +1,39 @@
-// AP name prefix
+// AP name prefix, shown when sensor is connected to new WiFi
 #define AP_PREFIX "AQBURK"
-// BURK_ID - change this in every device!!!
+// BURK_ID - make this different for every device!!! Will be shown as a part of AP name
 #define BURK_ID "800001"
-// Software version is YYMMDD (float)
+// Software version is YYMMDD (float). Increase this when doing new revisions of code
 #define DS_VERSION 190920
 
+/************************************************************************************************
+ * Connectivity settings
+ * =====================
+ * If you define BACKEND_URL (full URL including http:// prefix, HTTPS is not yet supported)
+ * this sketch tries to POST data to BACKEND_URL 
+ * 
+ * If you define MQTT settings (specially MQTT_SERVER)
+ * this sketch tries to connect MQTT broker and send the data there.
+ * 
+ * It is probably a very good idea to select just one of these, not both!
+ * 
+ * And if you can choose, take MQTT. It is much more efficient than HTTP in this case.
+ * However, it may be easier to prototype with HTTP using a local http server (on your laptop or something).
+ ************************************************************************************************/
+// HTTP settings
+/*
+#define BACKEND_URL "http://10.255.255.171:8000/dump"
+*/
+
 // MQTT settings
+/*
 #define MQTT_TOPIC "demosensor"
 #define MQTT_SERVER "mqtt.example.org"
 #define MQTT_PORT 1883
 #define MQTT_USER "mqtt_user_with_read-write_permission_to_topic"
 #define MQTT_PASSWORD "mqtt_password"
-#define MQTT_MAX_PACKET_SIZE 256 // For pubsubclient, default is 128 B
+*/
 
+#define MQTT_MAX_PACKET_SIZE 256 // For pubsubclient, default is 128 B
 #define SENSOR_SEND_MAX_DELAY 60000 // milliseconds, after this send data anyway even it has not changed at all, good values are between 1-10 minutes
 #define STATUS_SEND_DELAY 60000
 
